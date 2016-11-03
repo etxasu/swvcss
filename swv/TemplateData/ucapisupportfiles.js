026ee8d84475a76fae77fdb62f101236a5042975
Josef Shindler <josef.shindler@asu.edu>
2016-11-03 11:32:48 -0700

    Added LocalData to UCAPI

--- swv/TemplateData/ucapisupportfiles.js
+++ swv/TemplateData/ucapisupportfiles.js
@@ -12788,6 +12788,53 @@ define('api/snapshot/util/iframe', ['require'], function (require) {
     };
 });
 
+define('api/snapshot/LocalData', ['require'], function (require) {
+    var LocalData = {
+        getData: function (simId, key, onSuccess) {
+            var response = {
+                key: key,
+                value: null,
+                exists: false
+            };
+
+            try {
+                var simData = JSON.parse(window.sessionStorage.getItem(simId));
+                if (simData && simData.hasOwnProperty(key)) {
+                    response.value = simData[key];
+                    response.exists = true;
+                }
+            }
+            catch (err) {
+                console.warn('An error occurred while reading the date from sessionStorage.');
+            }
+            asyncResponse(response, onSuccess);
+        },
+        setData: function (simId, key, value, onSuccess) {
+            try {
+                var simData = JSON.parse(window.sessionStorage.getItem(simId)) || {};
+                simData[key] = value;
+                window.sessionStorage.setItem(simId, JSON.stringify(simData));
+            }
+            catch (err) {
+                console.warn('An error occurred while trying to save the data to sessionStorage.');
+            }
+            asyncResponse(null, onSuccess);
+        }
+    };
+
+    function asyncResponse(response, callback) {
+        setTimeout(sendResponse.bind(this, response, callback), 0);
+    }
+
+    function sendResponse(response, callback) {
+        callback(response);
+    }
+
+    return LocalData;
+});
+
+
+
 define('api/snapshot/SimCapiMessage',['require'],function(require) {
 
     var SimCapiMessage = function(options) {
