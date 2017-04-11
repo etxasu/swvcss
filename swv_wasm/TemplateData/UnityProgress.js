0d9a0c26f7831bc9b2bc11b5a264f487eabd2d0f
Josef Shindler <josef.shindler@asu.edu>
2017-04-11 11:37:12 -0700

    swv_wasm: Edited index

--- swv_wasm/TemplateData/UnityProgress.js
+++ swv_wasm/TemplateData/UnityProgress.js
@@ -1,24 +1,46 @@
-function UnityProgress(gameInstance, progress) {
-  if (!gameInstance.Module)
-    return;
-  if (!gameInstance.logo) {
-    gameInstance.logo = document.createElement("div");
-    gameInstance.logo.className = "logo " + gameInstance.Module.splashScreenStyle;
-    gameInstance.container.appendChild(gameInstance.logo);
-  }
-  if (!gameInstance.progress) {    
-    gameInstance.progress = document.createElement("div");
-    gameInstance.progress.className = "progress " + gameInstance.Module.splashScreenStyle;
-    gameInstance.progress.empty = document.createElement("div");
-    gameInstance.progress.empty.className = "empty";
-    gameInstance.progress.appendChild(gameInstance.progress.empty);
-    gameInstance.progress.full = document.createElement("div");
-    gameInstance.progress.full.className = "full";
-    gameInstance.progress.appendChild(gameInstance.progress.full);
-    gameInstance.container.appendChild(gameInstance.progress);
-  }
-  gameInstance.progress.full.style.width = (100 * progress) + "%";
-  gameInstance.progress.empty.style.width = (100 * (1 - progress)) + "%";
-  if (progress == 1)
-    gameInstance.logo.style.display = gameInstance.progress.style.display = "none";
+function init() {
+
+}
+
+function UnityProgress (dom) {
+	this.progress = 0.0;
+	this.message = "";
+	this.dom = dom;
+	var parent = dom.parentNode;
+
+	createjs.CSSPlugin.install(createjs.Tween);
+	createjs.Ticker.setFPS(60);
+
+	this.SetProgress = function (progress) { 
+		if (this.progress < progress) {
+			this.progress = progress;
+		}
+		if (progress == 1) {
+			this.SetMessage("Preparing...");
+			document.getElementById("spinner").style.display = "inherit";
+			document.getElementById("bgBar").style.display = "none";
+			document.getElementById("progressBar").style.display = "none";
+		} 
+		this.Update();
+	}
+
+	this.SetMessage = function (message) { 
+		this.message = message; 
+		this.Update();
+	}
+
+	this.Clear = function() {
+		document.getElementById("loadingBox").style.display = "none";
+	}
+
+	this.Update = function() {
+		var length = 200 * Math.min(this.progress, 1);
+		bar = document.getElementById("progressBar");
+		createjs.Tween.removeTweens(bar);
+		createjs.Tween.get(bar).to({width: length}, 500, createjs.Ease.sineOut);
+		bar.style.width = length + "px";
+		document.getElementById("loadingInfo").innerHTML = this.message;
+	}
+
+	this.Update();
 }
