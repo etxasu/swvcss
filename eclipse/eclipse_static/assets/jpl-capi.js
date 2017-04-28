ce9e5500ec0754b3b738164556b0831d616dd7a6
Josef Shindler <josef.shindler@asu.edu>
2017-04-28 10:46:23 -0700

    Ranger: Debug test

--- eclipse/eclipse_static/assets/jpl-capi.js
+++ eclipse/eclipse_static/assets/jpl-capi.js
@@ -14731,7 +14731,7 @@ define ('main',['require','jquery','ExtendedModel','api/snapshot/adapters/Backbo
             for (var i in changedAttributes)
             {
                 console.log("setting " + i + " to " + changedAttributes[i]);
-				
+				/*
 				if(i == "System.Show Shadow Labels")
 				{
 					if(typeof ranger_eclipse !== 'undefined')
@@ -14743,9 +14743,7 @@ define ('main',['require','jquery','ExtendedModel','api/snapshot/adapters/Backbo
 						console.log("Ranger not found");
 					}
 				}
-				
-				
-                //window.SendMessage("CAPI", "setValueFromJS", JSON.stringify({name: i, value: changedAttributes[i]}));
+				*/
             }
         }
     }
