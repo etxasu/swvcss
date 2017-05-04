412f80d0a6f82464aa180f43685e558c70fc1b6a
Josef Shindler <josef.shindler@asu.edu>
2017-05-04 15:32:01 -0700

    Ranger: Debugging test

--- eclipse/eclipse_static/assets/jpl-capi.js
+++ eclipse/eclipse_static/assets/jpl-capi.js
@@ -14764,7 +14764,7 @@ define ('main',['require','jquery','ExtendedModel','api/snapshot/adapters/Backbo
 						break;
 					case "System.Cone.PenumbraColor":
 						var _message = changedAttributes[i].split(",");
-						console.log(_message);
+						console.log(_message[0] + " " + _message[1] + " " + _message[2] + " " + _message[3]);
 						ranger_eclipse.update({setPenumbraColor: _message[0], _message[1], _message[2], _message[3]});
 						break;
 					case "System.Cone.UmbraColor":
