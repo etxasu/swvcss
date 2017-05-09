1bc8ad620acb4d667a0c50cb5569e19775fd5fd0
Josef Shindler <josef.shindler@asu.edu>
2017-05-09 13:22:43 -0700

    Ranger: Syntax fix

--- eclipse/eclipse_static/assets/public/eclipse/eclipse_manifest.js
+++ eclipse/eclipse_static/assets/public/eclipse/eclipse_manifest.js
@@ -1563,7 +1563,7 @@ var ranger_eclipse = {
 		locations.getLocationFromCoordinates(location_arr[0], location_arr[1], locations.addPin);
 		
 		//console.log(ranger_eclipse.update({getInfo: { "cameraState": { "id": "mainWindow"}}}));
-		console.log(ranger_eclipse.update({getInfo: { "cameraIds" }));
+		console.log(ranger_eclipse.update({getInfo: "cameraIds" }));
 		
 		receiveValueFromRanger("System.Locations.Last Clicked.Latitude", typeof location_arr[0], location_arr[0]);
 		receiveValueFromRanger("System.Locations.Last Clicked.Longitude", typeof location_arr[1], location_arr[1]);
